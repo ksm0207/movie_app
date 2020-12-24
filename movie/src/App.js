707e5841602c7a44d12a68a1a20bb18843ef5ce9
ksm0207 <ksm03071@naver.com>
2020-12-24 20:48:28 +0900

    movie app 추후 진행예정

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,8 +1,40 @@
 import React from 'react';
 
+
+function Coffee({name , age , job}){
+  return(
+  <div>
+    <h4>{name}</h4>
+    <h4>{age}</h4>
+    <h4>{job}</h4>
+  </div>
+  
+
+  )
+}
+
+const lists = [
+  {
+    name : "김성민",
+    age  : "26",
+    job  : "1"
+  }
+]
+
+
 function App() {
   return (
-    <h4>Hello</h4>
+    <div>
+      <h4>Hello</h4>
+      {
+        lists.map(cur =>(
+          <Coffee
+            name = {cur.name} age = {cur.age} job = {cur.job}
+          />
+          ))
+        }
+ 
+    </div>
   );
 }
 
